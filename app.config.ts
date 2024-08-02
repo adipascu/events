f1fe039bfd45ce2c615ecc469ce26cbc6c945e7b
Adrian Pascu <mail@adipascu.ro>
2024-08-02 16:24:32 +0200

    Add routeRules to Framer landing page

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    routeRules: {
+      "/**": { proxy: "https://smaller-items-983374.framer.app/**" },
+    },
+  },
+});
