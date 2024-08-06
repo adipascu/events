350d0112a1dcc007ec9eb7558f3c24d725e41c3d
Adrian Pascu <mail@adipascu.ro>
2024-08-06 02:48:12 +0200

    Add dev client API keys

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,12 @@
-export const API_KEY = "GRVWTE7Y7X2LHU45QQ";
-export const REDIRECT_URL = "https://matr.ics.adipascu.ro/oauth-redirect";
-// export const REDIRECT_URL = "http://localhost:3000/oauth-redirect"
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const API_KEY = IS_PRODUCTION
+  ? "GRVWTE7Y7X2LHU45QQ"
+  : "5PKBXOOMVOZ3F2B6QG";
+export const REDIRECT_URL = IS_PRODUCTION
+  ? "https://matrics.app/bridge/oauth-redirect"
+  : "http://localhost:3000/bridge/oauth-redirect";
 
 //secrets, do not leak in client JS!!
-export const CLIENT_SECRET =
-  "QNTUC55QFTLDY3NIW3NABQFO5EION25H6K24MS63JB7KXUPT7V";
+export const CLIENT_SECRET = IS_PRODUCTION
+  ? "QNTUC55QFTLDY3NIW3NABQFO5EION25H6K24MS63JB7KXUPT7V"
+  : "YCZUS6EX2NA5OTL75QV7PFT7JFLAORTGBOP3ZUSVCOLVGDU32V";
