08e47210d4febe7efb7c106f8029f33047f0af2d
Adrian Pascu <mail@adipascu.ro>
2024-08-06 02:44:31 +0200

    Move bridge concept to `/bridge`

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     routeRules: {
-      "/**": { proxy: "https://smaller-items-983374.framer.app/**" },
+      "/thank-you": {
+        proxy: "https://smaller-items-983374.framer.app/thank-you",
+      },
+      "/privacy-policy": {
+        proxy: "https://smaller-items-983374.framer.app/privacy-policy",
+      },
+      "/": { proxy: "https://smaller-items-983374.framer.app/" },
     },
   },
 });
