d3fe5c00b2e5ea070e6460a35d7378d764612a09
Adrian Pascu <mail@adipascu.ro>
2024-11-29 11:48:33 +0100

    Remove unused code

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-  server: {
-    routeRules: {
-      "/thank-you": {
-        proxy: "https://smaller-items-983374.framer.app/thank-you",
-      },
-      "/privacy-policy": {
-        proxy: "https://smaller-items-983374.framer.app/privacy-policy",
-      },
-      "/about": {
-        proxy: "https://smaller-items-983374.framer.app/about",
-      },
-      "/": { proxy: "https://smaller-items-983374.framer.app/" },
-    },
-  },
-});
+export default defineConfig();
--- src/config.ts
+++ src/config.ts
@@ -2,32 +2,6 @@ import { isServer } from "solid-js/web";
 
 export const IS_PRODUCTION = import.meta.env.PROD;
 
-export const EVENTBRITE_API_KEY = IS_PRODUCTION
-  ? "GRVWTE7Y7X2LHU45QQ"
-  : "5PKBXOOMVOZ3F2B6QG";
-
-export const EVENTBRITE_REDIRECT_URL = IS_PRODUCTION
-  ? "https://matrics.app/bridge/oauth-redirect"
-  : "http://localhost:3000/bridge/oauth-redirect";
-
-//secrets, do not leak in client JS!!
-export const EVENTBRITE_CLIENT_SECRET = IS_PRODUCTION
-  ? "QNTUC55QFTLDY3NIW3NABQFO5EION25H6K24MS63JB7KXUPT7V"
-  : "YCZUS6EX2NA5OTL75QV7PFT7JFLAORTGBOP3ZUSVCOLVGDU32V";
-
-export const FACEBOOK_API_KEY = IS_PRODUCTION
-  ? "1317565529216655"
-  : "1559461711350472";
-
-export const FACEBOOK_REDIRECT_URL = IS_PRODUCTION
-  ? "https://matrics.app/bridge/oauth/facebook"
-  : "http://localhost:3000/bridge/oauth/facebook";
-
-//secrets, do not leak in client JS!!
-export const FACEBOOK_CLIENT_SECRET = IS_PRODUCTION
-  ? "1c1a70d900243a0354541285133ab8b5"
-  : "d8f08b0288123d66d2f8d94c7b5fc4ff";
-
 export const COUCHDB_URL = (() => {
   const data = process.env.COUCHDB_URL;
   if (isServer) {
